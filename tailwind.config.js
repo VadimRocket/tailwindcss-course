d807b3e2e986bbad201b810214d86fd31b39e2d5
VadimRocket <vmartintestng@gmail.com>
2022-08-28 20:30:31 +0300

    added aspectRatio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio: {
+        '4/3': '4 / 3',
+      },
+    },
   },
   plugins: [],
 }
